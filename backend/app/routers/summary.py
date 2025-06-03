faefe121cd836280995e1729f08108d569873896
Brianna Lynn <Lynn_Brianna@bah.com>
2025-06-03 12:52:03 -0400

    Fixed some bugs and added dummy data for testing

--- backend/app/routers/summary.py
+++ backend/app/routers/summary.py
@@ -2,16 +2,18 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from app.database import get_db
 from app.models.summary import SummaryResponse
-from app.routers.alerts import get_alerts
+from app.routers.alerts import get_all_alerts
 
 router = APIRouter(prefix="/summary", tags=["summary"])
 
 
 @router.get("/", response_model=SummaryResponse)
 async def get_summary(db: Session = Depends(get_db)):
-    alerts = get_alerts(db=db)
+    alerts = get_all_alerts(db=db)
     # logic to generate summary
     #
-    summary = ""
+
+    # dummy data
+    summary = "; ".join([alert.name for alert in alerts])
 
     return {"description": summary}
